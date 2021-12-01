3d183da97ae1f742402d9c8223b20a825b03e4da
marisolcasanova <91167082+marisolcasanova@users.noreply.github.com>
2021-11-30 22:45:56 -0600

    Update db.sql

--- db.sql
+++ db.sql
@@ -5,8 +5,8 @@ CREATE DATABASE IF NOT EXISTS `pruebas_db` DEFAULT CHARACTER SET utf8 COLLATE ut
 USE `pruebas_db`;
 
 CREATE TABLE IF NOT EXISTS `alumnos_tb1` (
-  `id` int(11),
-  `nombre` varchar(50),
-  `email` varchar(50),
-  `edad` int(11)
+  `id` int(20),
+  `nombre` varchar(80),
+  `email` varchar(90),
+  `edad` int(20)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
