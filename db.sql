3e1ed01a3585224d0519de7935709d1cec48d3e8
marisolcasanova <marisolcasanova14@gmail.com>
2021-11-30 21:41:16 -0600

    database name

--- db.sql
+++ db.sql
@@ -4,7 +4,7 @@ GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
 CREATE DATABASE IF NOT EXISTS `pruebas_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `pruebas_db`;
 
-CREATE TABLE IF NOT EXISTS `alumnos_tbl` (
+CREATE TABLE IF NOT EXISTS `alumnos_tb1` (
   `id` int(11),
   `nombre` varchar(50),
   `email` varchar(50),
