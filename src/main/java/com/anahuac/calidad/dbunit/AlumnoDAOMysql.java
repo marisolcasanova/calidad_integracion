9764fa110ad69252cb291b74cd741049bd15fffc
marisolcasanova <marisolcasanova14@gmail.com>
2021-11-30 22:10:23 -0600

    db

--- src/main/java/com/anahuac/calidad/dbunit/AlumnoDAOMysql.java
+++ src/main/java/com/anahuac/calidad/dbunit/AlumnoDAOMysql.java
@@ -15,7 +15,7 @@ public class AlumnoDAOMysql implements AlumnoDAO {
 		Connection con=null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:33060/pruebas_db","root","");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas_db","root","");
 		}catch(Exception e) {System.out.println(e);}
 		return con;
 	}
